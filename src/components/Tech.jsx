293fec3354e91d1f6e032c018c735064d9d54529
Joel Thomas <joelthomas1103@gmail.com>
2024-09-01 11:33:35 +0530

    optimizing

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  // Determine if the device is a phone (you can adjust the width threshold as needed)
+  const isPhone = window.innerWidth <= 768;
+
+  // Filter technologies for mobile devices
+  const filteredTechnologies = isPhone
+    ? technologies.filter((tech) => 
+        ['Python', 'JavaScript', 'HTML', 'Tailwind CSS'].includes(tech.name)
+      )
+    : technologies;
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {filteredTechnologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
